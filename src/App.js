41d19f3fccf252a9145b67653b657751ab80b056
tony <tony@example.com>
2024-03-05 20:57:48 -0800

    simple e-commerce shopping cart made

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
-import "./App.css";
-import Loading from "./Loading";
-import Tours from "./Tours";
-import { useEffect, useState } from "react";
+
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  Navbar from './components/Navbar';
+import  Cart from './pages/cart/Cart';
+import  Shop  from './pages/shop/Shop';
+import { ShopContextProvider } from './context/ShopContext';
+
 
 function App() {
-  const url = "https://course-api.com/react-tours-project";
-  const [tours, setTours] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchTours = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(url);
-      const toursResponse = await response.json();
-      setLoading(false);
-      setTours(toursResponse);
-      console.log(tours);
-    } catch (e) {
-      setLoading(false);
-      console.log("Error while fetching: " + e);
-    }
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  const handleDelete = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
-
-  return (
-    <div className="App">
-      <h1 className="Heading">Our Tours</h1>
-      <div className="underline"></div>
-      {loading && <Loading />}
-      {!loading && <Tours tours={tours} handleDelete={handleDelete} />}
-    </div>
-  );
+  return <div className='App'>
+   <ShopContextProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
+    </ShopContextProvider>
+  </div>;
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <Link to="/">
+          <img src={logo} />
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="/"> SHOP </Link>
+        <Link to="/cart">Res</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
