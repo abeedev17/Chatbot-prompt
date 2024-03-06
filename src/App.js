521499743fd94d155fc8f6bb40d9f1a4238f086c
tony <tony@example.com>
2024-03-05 16:14:39 -0800

    Tour Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
-import React from "react";
+import "./App.css";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div>hello</div>;
+  const url = "https://course-api.com/react-tours-project";
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const toursResponse = await response.json();
+      setLoading(false);
+      setTours(toursResponse);
+      console.log(tours);
+    } catch (e) {
+      setLoading(false);
+      console.log("Error while fetching: " + e);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const handleDelete = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="Heading">Our Tours</h1>
+      <div className="underline"></div>
+      {loading && <Loading />}
+      {!loading && <Tours tours={tours} handleDelete={handleDelete} />}
+    </div>
+  );
 }
 
 export default App;
